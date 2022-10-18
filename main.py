e3bf7c25cdc804e8c7c5e8a2312b7b81a6b7ea9b
Jmarsaj <jerinsajimon@gmail.com>
2022-10-18 09:07:19 -0700

    main code
    transferring code from the old repository

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def imageCapture():                     #captures the frames from the video streaming
+    vid = cv2.VideoCapture(1)           #set as 0 for integrated webcam 1 for USB webcam
+    while True:
+        ret, frame = vid.read()
+        barcodeDecoder(frame)
+        cv2.imshow('Image', frame)
+        code = cv2.waitKey(10)
+        if code == ord('q'):
+            break
+
+def barcodeDecoder(image):
+
+    #img = cv2.imread(image)             #reads the image in numpy array
+    barcodes = decode(image)              #decodes barcode 
+    for obj in barcodes:       
+
+        (x, y, w, h) = obj.rect         #locating barcode in image
+        #cv2.rectangle(img, (x-10, y-10), (x+(w+10), y+(h+10)), (255, 0, 0), 2)
+
+        #if obj.data!="":
+        print(obj.data)
+        print(obj.type)
+
+imageCapture()
+#image = "barcode2.jpg"
+#barcodeDecoder(image)
