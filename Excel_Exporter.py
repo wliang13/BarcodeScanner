008e915f51c7dd006d9fafd49b102666a095c0b1
Jmarsaj <jerinsajimon@gmail.com>
2022-12-11 20:42:55 -0800

    updated excel_exporter so that it prints elements in the database

--- Excel_Exporter.py
+++ Excel_Exporter.py
@@ -11,53 +11,34 @@ from datetime import datetime
 # Add button on html to just do this function.
 # Find everything the barcode exports and add to headers here.
 
-def exportExcelSheet(rowList, barcodeList):
+def exportExcelSheet(entries):
     # Creates the xlsx file and dates it
     sTime = datetime.now().strftime("%H-%M-%S_-""_%m-%d-%Y")
     sDocumentName = 'BarcodeIndex-_' + sTime + '_.xlsx'
     xlsxWorkbook = xlsxwriter.Workbook(sDocumentName)
     xlsxWorksheet = xlsxWorkbook.add_worksheet()
+    date_format = xlsxWorkbook.add_format({'num_format': 'mm/dd/yyyy hh:mm'})
+    xlsxWorksheet.set_column(3,4,20)
 
     # Adds the bold font, use 'italics' or others in this section
     bold = xlsxWorkbook.add_format({'bold': True})
 
     # Header Goes Data Here If more is needed, this uses Row-Column as string in first argument.
-    xlsxWorksheet.write('A1', 'Row', bold)
-    xlsxWorksheet.write('B1', 'Barcode', bold)
-    #xlsxWorksheet.write('C1', 'Exp. D', bold)
-
-    """
-    # Test Input
-    elData = [
-        ['12349912', 'Tomatoes', '11/12'],
-        ['12747912', 'Carrots', '09/10'],
-        ['12851573', 'Cheese', '02/12']
-    ]
-    """
-
-    # iteration could be optimized dont bother
-    #iRow = 1
-    #iCol = 0
-    #for i, j in elData:
-    #    xlsxWorksheet.write(iRow, iCol, i)
-    #    xlsxWorksheet.set_column(iRow, iCol, 15)
-    #    xlsxWorksheet.write(iRow, iCol + 1, j)
-        #xlsxWorksheet.write(iRow, iCol + 2, k)
-    #    iRow += 1
+    xlsxWorksheet.write('A1', 'Factory', bold)
+    xlsxWorksheet.write('B1', 'Building', bold)
+    xlsxWorksheet.write('C1', 'Row', bold)
+    xlsxWorksheet.write('D1', 'Date Added', bold)
+    xlsxWorksheet.write('E1', 'Barcode', bold)
 
     iRow = 1
     iCol = 0
-    for row in rowList:
-        xlsxWorksheet.write(iRow, iCol, row)
+    for entry in entries:
+        xlsxWorksheet.write(iRow, iCol, entry.factory)
+        xlsxWorksheet.write(iRow, iCol+1, entry.building)
+        xlsxWorksheet.write(iRow, iCol+2, entry.content)
+        xlsxWorksheet.write(iRow, iCol+3, entry.date_created, date_format)
+        xlsxWorksheet.write(iRow, iCol+4, entry.barcode)
         iRow += 1
-    iRow = 1
-    iCol = 1
-    for barcode in barcodeList:
-        xlsxWorksheet.write(iRow, iCol, barcode)
-        iRow += 1
-
-
-
 
     xlsxWorkbook.close()
 
